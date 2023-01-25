b9adf4759f773ccd655f7aa573e9da4773848371
Blanca Ubis <ubis.blanca@gmail.com>
2023-01-25 11:36:38 +0100

    Change to DynamoDB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-const Database = require('better-sqlite3');
-const savedCards = [];
+//const Database = require('better-sqlite3');
+const CyclicDb = require('@cyclic.sh/dynamodb');
+
+//const savedCards = [];
 
 const server = express();
 
@@ -10,7 +12,25 @@ server.use(cors());
 server.use(express.json({ limit: '25mb' }));
 server.set('view engine', 'ejs');
 
-const db = new Database('./src/db/cards.db', { verbose: console.log });
+//const db = new Database('./src/db/cards.db', { verbose: console.log });
+const db = CyclicDb('fair-blue-dog-veilCyclicDB');
+
+let savedCards = db.collection('savedCards');
+
+const getCardById = async function (id) {
+  // create an item in collection with key id
+  // let cardId = await savedCards.set(id, result);
+
+  // get an item at key id from collection
+  let item = await savedCards.get(id);
+  return item;
+};
+
+const insertCard = async function (id, cardData) {
+  // create an item in collection with key id
+  let card = await savedCards.set(id, cardData);
+  return card;
+};
 
 //serverPort es igual al PORT (variable de entorno del sistema operativo) o si está vacío es igual a 4000
 const serverPort = process.env.PORT || 4000;
@@ -32,26 +52,31 @@ server.post('/card', (req, res) => {
   }
 
   if (success === true) {
-    const insertStmt = db.prepare(
-      'INSERT INTO userCards (palette,name,email,photo,phone,linkedin,github,job,salary,openToWork,additionalInfo) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
-    );
-    const result = insertStmt.run(
-      req.body.palette,
-      req.body.name,
-      req.body.email,
-      req.body.photo,
-      req.body.phone,
-      req.body.linkedin,
-      req.body.github,
-      req.body.job,
-      req.body.salary,
-      req.body.openToWork,
-      req.body.additionalInfo
-    );
+    // const insertStmt = db.prepare(
+    //   'INSERT INTO userCards (palette,name,email,photo,phone,linkedin,github,job,salary,openToWork,additionalInfo) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+    // );
+    // const result = insertStmt.run(
+    //   req.body.palette,
+    //   req.body.name,
+    //   req.body.email,
+    //   req.body.photo,
+    //   req.body.phone,
+    //   req.body.linkedin,
+    //   req.body.github,
+    //   req.body.job,
+    //   req.body.salary,
+    //   req.body.openToWork,
+    //   req.body.additionalInfo
+    // );
+
+    const id = uuid();
+
+    insertCard(id, req.body);
+
     const responseSuccess = {
       success: true,
-      //cambiamos la url de localhost a Cyclic.sh y le ponemos el ID que devuelve el INSERT
-      cardURL: `https://fair-blue-dog-veil.cyclic.app/card/${result.lastInsertRowid}`,
+      //cambiamos la url de localhost a Cyclic.sh y le ponemos el ID
+      cardURL: `https://fair-blue-dog-veil.cyclic.app/card/${id}`,
     };
     res.json(responseSuccess);
   } else {
@@ -67,9 +92,10 @@ server.get('/card/:id', (req, res) => {
   //guardamos el parámetro id de la url
   const id = req.params.id;
   //creamos la query
-  const query = db.prepare('SELECT * FROM userCards WHERE id = ?');
+  //const query = db.prepare('SELECT * FROM userCards WHERE id = ?');
   //ejecuto la query y me devuelve los datos de la tarjeta que correspondan con el id de la url
-  const userCard = query.get(id);
+  //const userCard = query.get(id);
+  const userCard = getCardById(id);
   console.log(userCard);
   const salaryText = () => {
     if (userCard.salary === '1') {
